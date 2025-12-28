7e4c52a9f72757240af3e8227fb3884d7a4647f4
danman14125 <danman14125@gmail.com>
2025-12-28 12:04:03 -0500

    B: now draws player sprite

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    #instantiate player
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -16,7 +21,10 @@ def main():
                 return
             
         screen.fill("black")
-        pygame.display.flip()
 
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000  # Limit to 60 FPS
+        
 if __name__ == "__main__":
     main()

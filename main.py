56a05baab10cf3df99e688aec55d2bad38d39e96
danman14125 <danman14125@gmail.com>
2025-12-31 12:45:24 -0500

    D: ASTEROIDS SPAWN!!!

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import pygame
 from constants import *
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -19,8 +28,9 @@ def main():
             if event.type == pygame.QUIT:
                 return 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000 
         
